9f82b4358d3daed81d92876b501a5f089cd88316
CNLih <lih0755@foxmail.com>
2022-05-29 13:02:54 +0800

    冲突测试

---
+++ Application/app/src/main/java/com/bytedance/application/bean/User.java
@@ -0,0 +1,27 @@
+package com.bytedance.application.bean;
+
+import androidx.databinding.BaseObservable;
+import androidx.databinding.Bindable;
+
+import com.example.application.BR;
+
+public class User extends BaseObservable {
+    private String name;
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    @Bindable
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+        //部分属性更新
+        notifyPropertyChanged(BR.name);
+        //全部更新
+        //notifyChange();
+    }
+}
