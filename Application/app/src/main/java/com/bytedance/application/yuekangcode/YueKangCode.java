0da0be85ddbedaad3f4111fc7a134131b43caa21
lblyl <81244960+lblyl@users.noreply.github.com>
2022-06-19 19:44:50 +0800

    feat

--- Application/app/src/main/java/com/bytedance/application/yuekangcode/YueKangCode.java
+++ Application/app/src/main/java/com/bytedance/application/yuekangcode/YueKangCode.java
@@ -1,13 +1,11 @@
 package com.bytedance.application.yuekangcode;
 
-import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.widget.RemoteViews;
-import android.widget.TextView;
 
 import com.bytedance.application.R;
 
@@ -15,19 +13,24 @@ import com.bytedance.application.R;
  * Implementation of App Widget functionality.
  */
 public class YueKangCode extends AppWidgetProvider {
-    private static final String URI = "weixin://dl/business/?t=QDZVQEO2z9f";
+    private static final String URL = "weixin://dl/business/?t=QDZVQEO2z9f";
 
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
-        CharSequence widgetText = context.getString(R.string.appwidget_text);
+        //CharSequence widgetText = context.getString(R.string.appwidget_text);
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.yue_kang_code);
-        views.setTextViewText(R.id.appwidget_text, widgetText);
-        Intent btIntent = new Intent().setAction(URI);
-        Intent btIntent1 = new Intent(Intent.ACTION_VIEW, Uri.parse("weixin://dl/business/?t=QDZVQEO2z9f"));
-        views.setOnClickFillInIntent(R.id.appwidget_text,btIntent1);
+        //iews.setTextViewText(R.id.appwidget_text, widgetText);
+        Intent btIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("weixin://dl/business/?t=QDZVQEO2z9f"));
+        views.setOnClickFillInIntent(R.id.appwidget_text,btIntent);
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+
     }
 
     @Override
@@ -36,6 +39,7 @@ public class YueKangCode extends AppWidgetProvider {
         for (int appWidgetId : appWidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId);
         }
+
     }
 
     @Override
